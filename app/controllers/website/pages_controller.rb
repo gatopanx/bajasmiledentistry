b9516d64b8b7a030f3afa218a1789a8858b1bad0
Ivan Rodriguez <ivaneduardorodriguezrodriguez@gmail.com>
2018-10-09 17:31:57 -0700

    kinda added structuer

--- app/controllers/website/pages_controller.rb
+++ app/controllers/website/pages_controller.rb
@@ -1,6 +1,58 @@
 module Website
   class PagesController < WebsiteController
     def home
+      @before_and_after_sets = [
+        {
+          before: 'https://placehold.it/400x300',
+          after: 'https://placehold.it/400x300',
+        },
+      ].map do |attribute_set|
+        OpenStruct.new(attribute_set)
+      end
+      @clients = [
+        {
+          name: 'Diego',
+          photo: './assets/person1.png',
+          testimonial: 'Me gustan las tortugas'
+        },
+        {
+          name: 'Melissa',
+          photo: './assets/person2.png',
+          testimonial: 'Me gustan las tortugas'
+        },
+        {
+          name: 'Genesis',
+          photo: './assets/person3.png',
+          testimonial: 'Me gustan las tortugas'
+        },
+        {
+          name: 'Alejandro',
+          photo: './assets/person4.png',
+          testimonial: 'Me gustan las tortugas'
+        },
+        {
+          name: 'Diego',
+          photo: './assets/person1.png',
+          testimonial: 'Me gustan las tortugas'
+        },
+        {
+          name: 'Melissa',
+          photo: './assets/person2.png',
+          testimonial: 'Me gustan las tortugas'
+        },
+        {
+          name: 'Genesis',
+          photo: './assets/person3.png',
+          testimonial: 'Me gustan las tortugas'
+        },
+        {
+          name: 'Alejandro',
+          photo: './assets/person4.png',
+          testimonial: 'Me gustan las tortugas'
+        },
+      ].map do |attribute_set|
+        OpenStruct.new(attribute_set)
+      end
     end
 
     def about_us
