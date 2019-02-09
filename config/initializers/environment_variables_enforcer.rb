bdbc809a57f177e37f0f98ba1995551a3cccee75
Ivan Rodriguez <ivaneduardorodriguezrodriguez@gmail.com>
2019-02-09 15:22:06 -0800

    wip

--- config/initializers/environment_variables_enforcer.rb
+++ config/initializers/environment_variables_enforcer.rb
@@ -1,30 +1,22 @@
-# ENFORCED_ENVIRONMENT_VARIABLES = [
-#   'AWS_ID',
-#   'AWS_KEY',
-#   'AWS_REGION',
-#   'AWS_UPLOADS_BUCKET_NAME',
-#   'DATABASE_TYPE',
-#   'DATABASE_HOST',
-#   'DATABASE_PORT',
-#   'DATABASE_USERNAME',
-#   'DATABASE_PASSWORD',
-#   'DEVELOPMENT_DATABASE_NAME',
-#   'TEST_DATABASE_NAME',
-#   'PRODUCTION_DATABASE_NAME'
-# ]
-#
-# if Rails.env == 'production'
-#   [
-#     'SECRET_KEY_BASE'
-#   ].each do |environment_variable|
-#     ENFORCED_ENVIRONMENT_VARIABLES << environment_variable
-#   end
-# end
-#
-# missing_environment_variables = ENFORCED_ENVIRONMENT_VARIABLES.select do |environment_variable|
-#   !(ENV[environment_variable])
-# end
-#
-# if missing_environment_variables.length > 0
-#   # raise StandardError.new("Missing enforced environment variables: #{missing_environment_variables.join(', ')}.")
-# end
+ENFORCED_ENVIRONMENT_VARIABLES = [
+  'AWS_ID',
+  'AWS_KEY',
+  'AWS_REGION',
+  'AWS_UPLOADS_BUCKET_NAME'
+]
+
+if Rails.env == 'production'
+  [
+    'SECRET_KEY_BASE'
+  ].each do |environment_variable|
+    ENFORCED_ENVIRONMENT_VARIABLES << environment_variable
+  end
+end
+
+missing_environment_variables = ENFORCED_ENVIRONMENT_VARIABLES.select do |environment_variable|
+  !(ENV[environment_variable])
+end
+
+if missing_environment_variables.length > 0
+  raise StandardError.new("Missing enforced environment variables: #{missing_environment_variables.join(', ')}.")
+end
